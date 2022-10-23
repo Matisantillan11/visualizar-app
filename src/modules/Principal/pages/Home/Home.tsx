dcf82e417ad53054826e6bfc3b61187acc8e192f
matisantillan11 <matias1.santillan@gmail.com>
2022-10-23 20:05:17 -0300

    adding titles for carousels on home screen and according margins

--- src/modules/Principal/pages/Home/Home.tsx
+++ src/modules/Principal/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import {BookCard} from '@visualizar/common/Cards/BookCard';
 import {FavoriteCard} from '@visualizar/common/Cards/FavoriteCard';
+import {STitle} from '@visualizar/common/Text';
 import React from 'react';
 import {ScrollView, Text, useWindowDimensions, View} from 'react-native';
 
@@ -7,12 +8,18 @@ export const Home = () => {
   const {width, height} = useWindowDimensions();
   return (
     <ScrollView style={{flex: 1}}>
+      <STitle style={{marginLeft: width * 0.05, fontSize: 20}}>
+        Libros destacados
+      </STitle>
       <ScrollView horizontal>
         <BookCard width={width * 0.4} height={height * 0.3} />
         <BookCard width={width * 0.4} height={height * 0.3} />
         <BookCard width={width * 0.4} height={height * 0.3} />
       </ScrollView>
-      <View>
+      <View style={{width, marginVertical: height * 0.05}}>
+        <STitle style={{marginLeft: width * 0.08, fontSize: 20}}>
+          Mis favoritos
+        </STitle>
         <FavoriteCard />
         <FavoriteCard />
       </View>
